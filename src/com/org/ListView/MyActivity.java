e85362847b939f7d6eed438a23cfa2198631f99d
AlvinChiou <alvinhsian@gmail.com>
2014-03-26 22:28:58 +0800

    Not Finish

--- src/com/org/ListView/MyActivity.java
+++ src/com/org/ListView/MyActivity.java
@@ -2,8 +2,10 @@ package com.org.ListView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class MyActivity extends Activity {
+    private ListView listView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
