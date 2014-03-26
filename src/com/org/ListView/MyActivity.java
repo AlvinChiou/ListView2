874520cb8ebdd92351e4582e1fbfb3c7641b4a73
AlvinChiou <alvinhsian@gmail.com>
2014-03-27 02:13:10 +0800

    Not finish

--- src/com/org/ListView/MyActivity.java
+++ src/com/org/ListView/MyActivity.java
@@ -2,11 +2,15 @@ package com.org.ListView;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 public class MyActivity extends Activity {
     private ListView listView;
-
+//    private class ImageTextAdapter extends BaseAdapter{
+//        private LayoutInflater layoutInflater;
+//    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
